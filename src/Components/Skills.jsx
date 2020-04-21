df6badd28d873cc1f1f75da8019524fee775314b
Michael Tan <michaelrstan@gmail.com>
2020-04-21 13:51:58 +1200

    Add list of skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 function Skills(){
     return <div>
         <h1>Skills</h1> 
-        <p>Java, JavaScript, HTML, React, CSS, C, C++</p>
+        <ul class="list-group">
+            <li class="list-group-item">Java</li>
+            <li class="list-group-item">JavaScript</li>
+            <li class="list-group-item">HTML</li>
+            <li class="list-group-item">React</li>
+            <li class="list-group-item">CSS</li>
+            <li class="list-group-item">C</li>
+            <li class="list-group-item">C++</li>
+        </ul>
+   
     </div>;
 
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./Components/App";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 //use npm start to start localhost:3000
+//https://www.anniefreeman.codes/
 
 
 ReactDOM.render(

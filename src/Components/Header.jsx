b9d7204f9da29acee7f62b373c8dece83149c7b2
Michael Tan <michaelrstan@gmail.com>
2020-07-03 21:14:59 +1200

    Put Navbar in top left corner of screen and make text larger and bolder

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,4 @@
 import React from "react";
-import {
-  Button,
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-} from "react-bootstrap";
 
 function Header() {
   return (
@@ -18,38 +10,6 @@ function Header() {
         <a href="#cv">CV</a>
         <a href="#contact">Contact</a>
       </div>
-
-      {/* <Navbar className="testHeader" bg="light" expand="lg" ></Navbar>*/}
-
-      {/* 
-      <Navbar className="testHeader" expand="lg">
-        <Navbar.Brand className="testHeader" href="#home"></Navbar.Brand>
-        <Navbar.Toggle
-          className="testHeader"
-          aria-controls="basic-navbar-nav"
-        />
-        <Navbar.Collapse className="testHeader" id="basic-navbar-nav">
-          <Nav className="mr-auto testHeader" activeKey="/home">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Item>
-              <Nav.Link href="/home">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#about">About</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#projects">Projects</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#cv">CV</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      */}
     </div>
   );
 }

d1b63598b13128e4c5affab0b57ca5c1f0aade0f
Michael Tan <michaelrstan@gmail.com>
2020-07-07 17:34:09 +1200

    Test server

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const app = express();
 app.use(favicon(__dirname + "/public/images/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
+//app.use(express.static(path.join(__dirname, "build")));
 app.get("/ping", function (req, res) {
   return res.send("pong");
 });
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  //res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 app.listen(port);
 

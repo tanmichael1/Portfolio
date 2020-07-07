4b55240ed1b94b864a45d399d6ef011926226aa1
Michael Tan <michaelrstan@gmail.com>
2020-07-07 17:38:33 +1200

    Test2 server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/ping", function (req, res) {
 });
 app.get("/*", function (req, res) {
   //res.sendFile(path.join(__dirname, "build", "index.html"));
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 app.listen(port);
 

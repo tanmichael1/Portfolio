34fa1ddbc49310123657426fc17fdf02f08c28a7
Michael Tan <michaelrstan@gmail.com>
2020-07-08 14:26:38 +1200

    Server changes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
@@ -16,11 +17,14 @@ app.get("/*", function (req, res) {
   //res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+
 //app.listen(port);
 
 app.listen(port, () => {
   console.log("Express server listening on port", port);
 });
+
+*/
 
 /*
   "scripts": {
@@ -47,3 +51,20 @@ app.listen(port, () => {
   console.log("server started " + port);
 });
 */
+
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + "/build/favicon.ico"));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/ping", function (req, res) {
+  return res.send("pong");
+});
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(port);

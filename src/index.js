1d462398c9272c417fc0be7388df51ee831e6396
Michael Tan <michaelrstan@gmail.com>
2020-07-16 20:41:21 +1200

    Set up express server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
-import "bootstrap/dist/css/bootstrap.min.css";
-//use npm start to start localhost:3000
 
-//Git
-
-ReactDOM.render(
-  <App />,
-
-  document.getElementById("root")
-);
-
-//<script type="module" src="app.js"></script>;
+ReactDOM.render(<App />, document.getElementById("root"));

92dff73cf612e453ee19cce33176c31b18427359
tanmichael1 <michaelrstan@gmail.com>
2021-06-18 11:48:14 +1200

    Remove Facebook Icon from Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,18 +1,10 @@
 import React from "react";
-import { IoLogoLinkedin, IoLogoFacebook, IoLogoGithub } from "react-icons/io";
+import { IoLogoLinkedin, IoLogoGithub } from "react-icons/io";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footerIcon">
-        <a
-          className="link"
-          href="https://www.facebook.com/profile.php?id=100004455215502"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <IoLogoFacebook size="3.5rem" color="white" />
-        </a>
         <a
           className="link"
           href="https://www.linkedin.com/in/michael-tan-287ba4192/"
